14a0e3b36b240f2577f4254b846fcc0fbf22d5ee
郭宇沫 <damozi@guoyumodeMacBook-Pro.local>
2019-05-16 21:20:56 +0800

    商户页面

---
+++ public/js/utils/stringUtils.js
@@ -0,0 +1,14 @@
+
+define([''], function () {
+    return {
+        // 判断字符串非空
+        isNotEmpty: function(str) {
+            return str !== "" && str !== null && str !== undefined;
+        },
+
+        // 判断字符串为空
+        isEmpty: function(str) {
+            return str === "" || str === null || str === undefined;
+        }
+    }
+});
